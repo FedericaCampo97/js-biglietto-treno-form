e9d85b1dcb38e0bc8b73bf11d85e7507a83d402f
FedericaCampo97 <federica.campo97@gmail.com>
2023-08-24 17:08:36 +0200

    print on page name and type of ticket

--- assets/main.js
+++ assets/main.js
@@ -9,17 +9,24 @@ DOMPulsante.addEventListener('click' , function(){
     const kmUtente = document.getElementById('km').value
     //seleziono gli anni
     const anniUtente = document.getElementById('age').value
+    //Dichiaro variabile tipologia biglietto
+    let typeticket
     //calcolo il prezzo per km
     let prezzoBiglietto = kmUtente * 0.21
     //condizioni con i vari sconti
     let prezzoFinale;
     if(anniUtente < 18) {
         prezzoFinale = prezzoBiglietto - ((prezzoBiglietto * 20) / 100 )
+        typeticket = 'Biglietto Ridotto (under 18)'
     } else if(anniUtente >= 65) {
          prezzoFinale = prezzoBiglietto - ((prezzoBiglietto * 40) / 100 )
+         typeticket = 'Biglietto Ridotto (over 65)'
     } else {
-        prezzoFinale = prezzoBiglietto
+        prezzoFinale = prezzoBiglietto 
+        typeticket = 'Biglietto Standard'
     }
     //stampo in console
     console.log(prezzoFinale)
+    document.getElementById('fullNameTicket').innerHTML = fullName
+    document.getElementById('type').innerHTML = typeticket
 });
